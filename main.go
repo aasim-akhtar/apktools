9a83aacc43144a1450262c672dc69a42abc7ec53
aasim-akhtar <xpaasim@gmail.com>
2022-03-16 15:05:03 +0530

    Added dummyapktool

--- main.go
+++ main.go
@@ -18,14 +18,15 @@ func main() {
 func startServer() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/apktool", fileUpload).Methods("POST")
-	r.HandleFunc("/api/apktool/uploadHandler", rest_apktool).Methods("POST")
+	r.HandleFunc("/api/fileupload", fileUpload).Methods("POST")
+	r.HandleFunc("/api/apktool", rest_apktool).Methods("POST")
+	r.HandleFunc("/api/dummyapktool",dummyApkTool).Methods("POST")
 
 	fmt.Println("SERVER STARTED AT PORT 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
-func apktool(f *os.File) {
+func dummyApkTool(w http.ResponseWriter, r *http.Request) {
 	if _, err := os.Stat("tools/Voice_Recorder_v54.1_apkpure.com"); !os.IsNotExist(err) {
 		// path/to/whatever exists
 		fmt.Println("Deleting existing Decoded files")
@@ -35,13 +36,13 @@ func apktool(f *os.File) {
 	// java -jar apktool.jar d ../../../apk/Voice_Recorder_v54.1_apkpure.com.apk
 
 	path := "tools"
-	// vr := Voice_Recorder_v54.1_apkpure.com.apk
-	cmd := "java -jar apktool.jar d ../" + f.Name()
+	vr := "Voice_Recorder_v54.1_apkpure.com.apk"
+	cmd := "java -jar apktool.jar d ../apk/" + vr
 
 	cmdStruct := exec.Command("cmd.exe", "/c", cmd)
 	cmdStruct.Dir = path
 	fmt.Println(cmdStruct.Args)
-	cmdStruct.Stdout = os.Stdout
+	cmdStruct.Stdout = w
 	err := cmdStruct.Start()
 
 	if err != nil {
@@ -56,7 +57,7 @@ func apktool(f *os.File) {
 		fmt.Println("apktool completion error", err)
 	}
 	fmt.Println("Reached End of Command")
-
+	fmt.Fprintf(w,"Task Completed Sucessfully")
 }
 
 func fileUpload(w http.ResponseWriter, r *http.Request) {
@@ -89,7 +90,7 @@ func fileUpload(w http.ResponseWriter, r *http.Request) {
 
 	tempFile.Write(fileBytes)
 
-	fmt.Fprintf(w, "File Uploaded sucessfully")
+	fmt.Fprintf(w, "File Uploaded sucessfully\n")
 
 	// rest_apktool(tempFile)
 }
@@ -104,7 +105,10 @@ func rest_apktool(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(file.Name())
 	fmt.Fprintf(w,"File Uploaded Sucessfully")
-	apktool(file)
+	apktool(file,w)
+	// w.Header().Set("Content-Type","application/zip")
+	// w.Write(archive(file.Name()))
+	fmt.Fprintf(w,"Task Completed Sucessfully")
 
 }
 
@@ -128,6 +132,10 @@ func uploadHandler (w http.ResponseWriter, r *http.Request) (*os.File , error){
 		return nil, err
 	}
 
+	// Detect Content Type
+	// contentType := http.DetectContentType(fileBytes)
+	// fmt.Fprintf(w,contentType)
+
 	fPath += handler.Filename
 	f,err := os.OpenFile(fPath, os.O_RDONLY | os.O_CREATE,0644)
 	if err != nil {
@@ -145,3 +153,63 @@ func uploadHandler (w http.ResponseWriter, r *http.Request) (*os.File , error){
 
 	return f, err
 } 
+
+
+func apktool(f *os.File,w http.ResponseWriter) {
+	c := "tools/"+f.Name()+"_src"
+	fmt.Println(c)
+	if _, err := os.Stat(c); !os.IsNotExist(err) {
+		// path/to/whatever exists
+		fmt.Println("Deleting existing Decoded files")
+		exec.Command("cmd.exe", "/c", "rmdir", "/q", "/s", "D:/Learning/Cyfinoid/GO Codes/apktools/tools/apk/Voice_Recorder_v54.1_apkpure.com.apk_src").Output()
+
+	}
+	// java -jar apktool.jar d ../../../apk/Voice_Recorder_v54.1_apkpure.com.apk
+
+	path := "tools"
+	// vr := Voice_Recorder_v54.1_apkpure.com.apk
+	SRC_PATH := f.Name() + "_src"
+	cmd := "java -jar apktool.jar d ../" + f.Name() + " -o " + SRC_PATH
+
+	cmdStruct := exec.Command("cmd.exe", "/c", cmd)
+	cmdStruct.Dir = path
+	fmt.Println(cmdStruct.Args)
+	cmdStruct.Stdout = os.Stdout
+	cmdStruct.Stderr = os.Stderr
+
+	err := cmdStruct.Start()
+
+	if err != nil {
+		fmt.Println("Unable to start apktool", err)
+		return
+	}
+
+	// fmt.Println(string(out))
+
+	err = cmdStruct.Wait()
+	if err != nil {
+		fmt.Println("apktool completion error", err)
+	}
+	fmt.Println("Reached End of Command")
+
+
+}
+
+func archive (f string) ([]byte) {
+	fmt.Println("Adding file to archive")
+	fmt.Println(f)
+	p :=  f + "_src/"
+	cmdStruct := exec.Command("tools/7z.exe","a",p)
+	cmdStruct.Stdout = os.Stdout
+	cmdStruct.Stderr = os.Stderr
+	err := cmdStruct.Start()
+	if err != nil {
+		fmt.Println("Error adding to archive",err)
+	}
+	err = cmdStruct.Wait()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return nil
+
+}
